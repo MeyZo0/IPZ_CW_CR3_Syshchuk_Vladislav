77a7b1637366c1e4796d859e1c0de65934fad370
MeyZo0 <vgadstrev@gmail.com>
2024-03-13 15:24:39 +0200

    добавив зданій фон

--- app/src/main/java/ua/edu/lntu/cw_cr3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_cr3/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -11,15 +13,21 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -45,7 +53,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainWindow() {
-    Column {
+    Column (horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,) {
         Text(
             text = "30 Days of Wellenss",
             fontSize = 30.sp,
@@ -67,7 +76,13 @@ fun MainWindow() {
 
 @Composable
 fun item1 () {
-    Column {
+    Column (modifier = Modifier
+            .background(Color.Gray)
+            .clip(shape = RoundedCornerShape(6.dp))
+            .padding(15.dp)
+    )
+
+    {
 
         Text(
             text = "day 1",
@@ -92,7 +107,11 @@ fun item1 () {
 
 @Composable
 fun item2 () {
-    Column {
+    Column (modifier = Modifier
+        .background(Color.Gray)
+        .clip(shape = RoundedCornerShape(6.dp))
+        .padding(15.dp)
+    ) {
 
         Text(
             text = "day 2",
@@ -117,7 +136,11 @@ fun item2 () {
 
 @Composable
 fun item3 () {
-    Column {
+    Column (modifier = Modifier
+        .background(Color.Gray)
+        .clip(shape = RoundedCornerShape(6.dp))
+        .padding(15.dp)
+    ) {
 
         Text(
             text = "day 3",
